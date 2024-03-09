0ad98851c8af82658eb9136c711c46f4819fdcbb
Guddyx <55354502+Guddyx@users.noreply.github.com>
2024-03-09 14:01:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,17 +13,19 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-# 移除要替换的包
+# 移除要替换的包,添加luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/utils/v2dat
-
-# 更新golang
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
-# 添加luci-app-mosdns
 git clone https://github.com/sbwml/luci-app-mosdns.git mosdns
 cp -rf mosdns/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
-cp -rf mosdns/luci-app-mosdns/v2dat feeds/packages/utils/v2dat
+
+
+
+# 更新golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# 添加luci-app-mosdns
+#rm -rf feeds/packages/utils/v2dat
+#cp -rf mosdns/luci-app-mosdns/v2dat feeds/packages/utils/v2dat
 
 
